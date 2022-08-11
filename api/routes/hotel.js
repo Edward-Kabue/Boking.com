3d6fa55c0dcc96423e5c2159d1f4826b3d828175
Baricho <ekabue0@gmail.com>
2022-08-11 11:30:30 +0300

    Configure crud controllers for the hotels data

--- api/routes/hotel.js
+++ api/routes/hotel.js
@@ -1,67 +1,32 @@
 import express from 'express';
+//use conrollers for crud operations.
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from '../controllers/hotel.js';
 
-import Hotel from '../models/Hotel.js';
-import { createError } from '../utils/error.js';
 const router = express.Router();
 
 //create
-router.post('/', async (req, res) => {
-  const newHotel = new Hotel(req.body);
-
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (err) {
-    res.status(500).json;
-  }
-});
+router.post('/', createHotel);
 
 //update
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    res.status(500).json;
-  }
-});
+router.put('/:id', updateHotel);
 //delete
-router.delete('/:id', async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json('hotel deleted');
-  } catch (err) {
-    res.status(500).json;
-  }
-});
+router.delete('/:id', deleteHotel);
 
 //get
-router.get('/:id', async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel);
-  } catch (err) {
-    res.status(500).json;
-  }
-});
+router.get('/:id', getHotel);
 //getall
-router.get('/', async (req, res, next) => {
+router.get(
+  '/',
+  getHotels
   // const failed = true;
 
   // if (failed) return next(createError(401, 'No Authentication'));
-
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (err) {
-    next(err);
-  }
-});
+);
 
 export default router;
